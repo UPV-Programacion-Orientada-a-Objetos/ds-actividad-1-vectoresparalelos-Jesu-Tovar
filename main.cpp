d561bbec7a2592efabe9c961f0a35517ced18d95
Suzu <2430344@upv.edu.mx>
2025-09-22 12:03:36 -0600

    primer intento de implementarle el menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+void registrar(std::string nom[], std::string pa[], std::string dis[], std::string ge[]){
+
+}
 int main() {
+
+int pepe = 1;
+
+    while(pepe <= 5){
 
     std::string nombreArchivo = "atletas.txt";
     std::ifstream archivo(nombreArchivo);
@@ -19,6 +28,25 @@ int main() {
         }
         archivo.close();
     }
+
+    std::string nombre[pepito], pais[pepito], disciplina[pepito], genero[pepito];
+    int medalla[pepito];
+
+    std::cout << "----- M E N U ----- \n1)Registrar \n6)Salir" << std::endl;
+    std::cin >> pepe;
+
+    while(pepe >= 7){
+        std::cout << "Opcion incorrecta, seleccione otra" << std::endl;
+        std::cin >> pepe;
+    }
+
+    if(pepe == 1){
+        Registrar()
+    }
+
+
+    }
+
 
     return 0;
 }
