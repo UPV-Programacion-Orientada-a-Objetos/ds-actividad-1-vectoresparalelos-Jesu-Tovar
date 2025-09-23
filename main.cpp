97c5c20bb990f4ac58443209abc84bef7a460028
Suzu <2430344@upv.edu.mx>
2025-09-22 21:46:34 -0600

    NOSE PQ NO ME JALABA EL NOMBRE, TENIA QUE PONERLE EL CIN.IGNORE

--- main.cpp
+++ main.cpp
@@ -39,40 +39,55 @@ void registrarAtleta() {
     std::cout << "Atleta registrado.\n";
 }
 
+void buscarAtleta() {
+    std::string nombreBuscar;
+    std::cout << "Nombre del atleta a buscar: ";
+    std::getline(std::cin, nombreBuscar);
+
+    std::ifstream in(nombreArchivo);
+    std::string linea;
+    std::getline(in, linea);
+    bool encontrado = false;
+
+    while (std::getline(in, linea)) {
+        std::stringstream ss(linea);
+        std::string nombre, pais, disciplina, genero, medStr;
+        std::getline(ss, nombre, ',');
+        std::getline(ss, pais, ',');
+        std::getline(ss, disciplina, ',');
+        std::getline(ss, genero, ',');
+        std::getline(ss, medStr, ',');
+
+        if (nombre == nombreBuscar) {
+            std::cout << "Pais: " << pais << " | Medallas: " << medStr << std::endl;
+            encontrado = true;
+        }
+    }
+    if (!encontrado) std::cout << "Atleta no encontrado.\n";
+    in.close();
+}
+
+
+
 int main() {
+    crearArchivo();
+    int pepe = 0;
 
 
     std::cout << "----- M E N U ----- \n1)Registrar \n2)Buscar \n3)Calcular medallas por pais \n4)Mejor atleta \n5)Mostrar todos \n6)Salir" << std::endl;
     std::cin >> pepe;
+    std::cin.ignore();
     switch (pepe) {
-            case 1:
-                
-                break;
-            case 2:
-                
-                break;
-            case 3:
-                
-                break;
-            case 4:
-                
-                break;
-            case 5:
-                
-                break;
-            case 6:
-                std::cout << "llegale" << std::endl;
-                break;
+        case 1: registrarAtleta(); break;
+        case 2: buscarAtleta(); break;
+        case 6: std::cout << "Adios.\n"; break;
+        default: std::cout << "Opcion incorrecta.\n"; break;
         }
 
     while(pepe >= 7){
         std::cout << "Opcion incorrecta, seleccione otra" << std::endl;
         std::cin >> pepe;
     }
-
-
-    }
-
-
+    
     return 0;
-}
+    }
